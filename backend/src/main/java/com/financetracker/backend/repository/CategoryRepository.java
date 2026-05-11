cb66979c89de3e3b16dac7cedaf7950f5905aabe
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-11 11:37:50 +0530

    update

--- backend/src/main/java/com/financetracker/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/financetracker/backend/repository/CategoryRepository.java
@@ -4,8 +4,10 @@ import com.financetracker.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByUser(User user);
     List<Category> findByUserAndType(User user, String type);
+    Optional<Category> findByIdAndUser(Long id, User user);
 }
